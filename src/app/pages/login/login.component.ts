bb69e6bda44dfa2ea6bff135b575147f88a44f14
Antonio Alves <antoniocunha9610@gmail.com>
2024-06-25 18:56:59 -0300

    feat:impletando método de verificação senha e nome

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
 interface LoginForm {
   email: FormControl,
   password: FormControl
@@ -14,6 +16,9 @@ interface LoginForm {
     ReactiveFormsModule,
     PrimaryInputComponent
   ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -21,12 +26,30 @@ export class LoginComponent {
 
   loginForm!: FormGroup;
 
-  constructor() {
+  constructor(
+    private router:Router,
+  private loginService:LoginService) {
 
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
+
+   }
+
+   submitOnLogin(){
+    console.log(this.loginForm.value);
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next:(value) => console.log("sucesso"),
+      error:(error) => console.log("error")
+
+
+    })
+   }
+
+   navigateOnLogin(){
+    console.log(this.loginForm.value+ "logadooo");
+    this.router.navigate(["/singup"])
 
    }
 }
